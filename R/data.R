9d63e972d1965cf1be4ee58bff231454c297df9c
Matt Cowgill <mattcowgill@gmail.com>
2020-09-10 11:30:36 +1000

    add butlin tables

--- R/data.R
+++ R/data.R
@@ -1,4 +1,112 @@
 #' Long-run labour force data
 #' From Butlin
-#' @source Butlin
+#' @source Butlin 1977 'A Preliminary Annual Database 1900/01 to 1973/74'
 "lr_lfs"
+
+#'
+
+#' Table A1 of Butlin, Dixon and Lloyd 2014
+#'
+#' GDP and sectoral shares, 1801-2010.
+#'
+#' @format A tibble with six variables:
+#' \describe{
+#'   \item{year}{Financial year ending, eg. '2007' means the '2006-07' financial year. Sectoral shares prior to 1900 are calendar years.}
+#'   \item{rgdp}{Real GDP, \$ million, constant 2010-11 prices}
+#'   \item{ag_share}{Agriculture's share of GDP/GNP, current prices, in \%. Until 1861, includes pastoral and non-pastoral primary production.}
+#'   \item{mining_share}{Mining's share of GDP/GNP, current prices, in \%}
+#'   \item{manuf_share}{Manufacturing's share of GDP/GNP, current prices, in \%. Until 1861, includes private construction.}
+#'   \item{other_share}{Other industries' share of GDP/GNP, current prices, in \%}
+#' }
+#' @source \url{https://doi.org/10.1017/CHO9781107445222.033}. See PDF for detailed notes and sources.
+
+"butlin_t1"
+
+#' Table A2 of Butlin, Dixon and Lloyd 2014
+#'
+#' Population, migration, labour force and unemployment, 1800-2010.
+#'
+#' @format A tibble with 10 variables:
+#' \describe{
+#'   \item{year}{Mix of calendar and financial year. Population is December each year; unemployment rate is financial year ended.}
+#'   \item{males}{Male population}
+#'   \item{females}{Female population}
+#'   \item{persons}{Total population}
+#'   \item{sex_ratio}{males \ females * 100}
+#'   \item{net_migration_share_of_increase}{Share of population increase accounted for by net migration, per cent}
+#'   \item{perc_pop_born_overseas}{Proportion of population born overseas, per cent}
+#'   \item{labour_force}{Number of persons in the labour force, thousands}
+#'   \item{unemp_rate}{Unemployment rate, as a percentage of the labour force}
+#'   \item{part_rate}{Participation rate; labour force as a percentage of total (not working age) population}
+#' }
+#' @source https://doi.org/10.1017/CHO9781107445222.033. See PDF for detailed notes and sources.
+
+"butlin_t2"
+
+#' Table A3 of Butlin, Dixon and Lloyd 2014
+#' Exports, imports, openness and current account deficit, 1822-2010
+#'
+#' @format A tibble with 8 variables:
+#' \describe{
+#'   \item{year}{Prior to 1913: calendar year. 1914 onwards: financial year ended.}
+#'   \item{exports}{Exports, in millions of dollars, nominal}
+#'   \item{imports}{Imports, in millions of dollars, nominal}
+#'   \item{balance_of_trade}{Balance of trade, exports minus imports, in millions of dollars, nominal}
+#'   \item{openness_ratio}{Openness ratio as percentage of GDP; the sum of exports and imports as a percentage of GDP}
+#'   \item{current_account_m}{Current account balance in millions of dollars, nominal}
+#'   \item{current_account_percgdp}{Current account balance as a percentage of GDP}
+#'   \item{terms_of_trade}{Terms of trade; index, 1966-67 = 100.}
+#'
+#' }
+#'
+#' @source https://doi.org/10.1017/CHO9781107445222.033. See PDF for detailed notes and sources.
+
+"butlin_t3"
+
+#' Table A4 of Butlin, Dixon and Lloyd 2014
+#' Exports, imports, openness and current account deficit, 1822-2010
+#'
+#' @format A tibble with x variables:
+#' \describe{
+#'   \item{}{}
+#' }
+#'
+#' @source https://doi.org/10.1017/CHO9781107445222.033. See PDF for detailed notes and sources.
+
+"butlin_t3"
+
+#' Table A3 of Butlin, Dixon and Lloyd 2014
+#' Exports, imports, openness and current account deficit, 1822-2010
+#'
+#' @format A tibble with x variables:
+#' \describe{
+#'   \item{}{}
+#' }
+#'
+#' @source https://doi.org/10.1017/CHO9781107445222.033. See PDF for detailed notes and sources.
+
+"butlin_t3"
+
+#' Table A3 of Butlin, Dixon and Lloyd 2014
+#' Exports, imports, openness and current account deficit, 1822-2010
+#'
+#' @format A tibble with x variables:
+#' \describe{
+#'   \item{}{}
+#' }
+#'
+#' @source https://doi.org/10.1017/CHO9781107445222.033. See PDF for detailed notes and sources.
+
+"butlin_t3"
+
+#' Table A3 of Butlin, Dixon and Lloyd 2014
+#' Exports, imports, openness and current account deficit, 1822-2010
+#'
+#' @format A tibble with x variables:
+#' \describe{
+#'   \item{}{}
+#' }
+#'
+#' @source https://doi.org/10.1017/CHO9781107445222.033. See PDF for detailed notes and sources.
+
+"butlin_t3"
--- data-raw/put-data.R
+++ data-raw/put-data.R
@@ -1,5 +1,24 @@
 
 lr_lfs <- readr::read_csv("data-raw/lr_lf_time_series.csv")
+
+
+raw_csv_filenames <- list.files("data-raw")[tools::file_ext(list.files("data-raw")) == "csv"]
+butlin_filenames <- raw_csv_filenames[grepl("butlin_", raw_csv_filenames)]
+
+butlin <- purrr::map(paste0("data-raw/", butlin_filenames),
+                          readr::read_csv)
+
+butlin <- setNames(butlin, tools::file_path_sans_ext(butlin_filenames))
+
+list2env(butlin, .GlobalEnv)
+
 usethis::use_data(lr_lfs,
+                  butlin_t1,
+                  butlin_t2,
+                  butlin_t3,
+                  butlin_t4,
+                  butlin_t5,
+                  butlin_t6,
+                  butlin_t7,
                   internal = FALSE,
                   overwrite = TRUE)
